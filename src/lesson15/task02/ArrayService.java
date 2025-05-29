f573f8c4c82dbb15b412bca99b6b9e21654b5288
“Yuriy <dolgih.yv@gmail.com>
2025-05-29 17:09:15 +0200

    lesson 15 - Arrays (small change in algorithm)

--- src/lesson15/task02/ArrayService.java
+++ src/lesson15/task02/ArrayService.java
@@ -3,7 +3,7 @@ package lesson15.task02;
 public class ArrayService {
     public static boolean isArrayIncSequence(int[] srcArr){
         for (int i = 0; i < srcArr.length-1; i++) {
-            if (srcArr[i] > srcArr[i+1]){
+            if (srcArr[i] >= srcArr[i+1]){
                 return false;
             }
         }
