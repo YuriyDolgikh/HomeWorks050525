41123751f461acdcbd1fb7733b4ee923054d1d10
“Yuriy <dolgih.yv@gmail.com>
2025-06-14 13:36:54 +0200

    lesson21 - Junit

--- test/java/lesson21/task1EvenNumbers/EvenNumbersTest.java
+++ test/java/lesson21/task1EvenNumbers/EvenNumbersTest.java
@@ -7,19 +7,32 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 class EvenNumbersTest {
 
-    int[] numbersCommonCase = {1, 22, -32, 0, 52, 161, -77, 58, 99, -201};
-    int[] numbersAllEven = {4, 66, 34, -346, -98, 0};
-    int[] numbersAllOdd = {5, 67, 213, 49, -333};
     EvenNumbers evenNumbers = new EvenNumbers();
 
     @Test
-    void testGetEvenNumbers() {
+    void testGetEvenNumbersCommonCase() {
+        int[] numbersCommonCase = {1, 22, -32, 0, 52, 161, -77, 58, 99, -201};
         int[] resultArrayCommonCase = {22, -32, 0, 52, 58};
-        int[] resultArrayAllEven = {4, 66, 34, -346, -98, 0};
-        int[] resultArrayAllOdd = {};
         assertArrayEquals(resultArrayCommonCase, evenNumbers.getEvenNumbers(numbersCommonCase));
+    }
+
+    @Test
+    void testGetEvenNumbersAllEven() {
+        int[] numbersAllEven = {4, 66, 34, -346, -98, 0};
+        int[] resultArrayAllEven = {4, 66, 34, -346, -98, 0};
         assertArrayEquals(resultArrayAllEven, evenNumbers.getEvenNumbers(numbersAllEven));
+    }
+
+    @Test
+    void testGetEvenNumbersAllOdd() {
+        int[] numbersAllOdd = {5, 67, 213, 49, -333};
+        int[] resultArrayAllOdd = {};
         assertArrayEquals(resultArrayAllOdd, evenNumbers.getEvenNumbers(numbersAllOdd));
+    }
+
+    @Test
+    void testGetEvenNumbersArrayIsNull() {
         assertNull(evenNumbers.getEvenNumbers(null));
     }
+
 }
--- test/java/lesson21/task2Average/AverageTest.java
+++ test/java/lesson21/task2Average/AverageTest.java
@@ -6,16 +6,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AverageTest {
 
-    double[] sourceArray = {45, 678, 234, 879, 45, 23, -3, 0, 34};
-    double[] sourceEmptyArray = {};
-    double[] sourceNull = null;
     Average average = new Average();
 
     @Test
-    void testGetAverage() {
+    void testGetAverageArrayIsExists() {
+        double[] sourceArray = {45, 678, 234, 879, 45, 23, -3, 0, 34};
         double result = 215.0;
         assertEquals(result, average.getAverage(sourceArray));
+    }
+
+    @Test
+    void testGetAverageArrayIsEmpty() {
+        double[] sourceEmptyArray = {};
+        double result = 215.0;
         assertTrue(Double.isNaN(average.getAverage(sourceEmptyArray)));
+    }
+
+    @Test
+    void testGetAverageArrayIsNull() {
+        double[] sourceNull = null;
+        double result = 215.0;
         assertThrows(NullPointerException.class, () -> average.getAverage(sourceNull));
     }
 }
