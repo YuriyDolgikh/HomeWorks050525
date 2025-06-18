b54e7011cbeb2d2d348346b9979c58dede3db62c
“Yuriy <dolgih.yv@gmail.com>
2025-06-18 21:53:02 +0200

    lesson23 - cosmetic change

--- src/lesson23/task1/ArrayService.java
+++ src/lesson23/task1/ArrayService.java
@@ -4,7 +4,7 @@ import utils.UserInputStatic;
 
 public class ArrayService {
 
-    public int[] sort(int [] array) {
+    public int[] sort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] < array[j]) {
@@ -17,20 +17,20 @@ public class ArrayService {
         return array;
     }
 
-   public int[] createArrayFromUserInput(){
+    public int[] createArrayFromUserInput() {
         return new int[UserInputStatic.inputInt("Please enter the size of the array:")];
-   }
+    }
 
-   public int[] fillArrayFromUserInput(int [] array){
+    public int[] fillArrayFromUserInput(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            array[i] =  UserInputStatic.inputInt("Please enter the value for the element " + i + ":");
+            array[i] = UserInputStatic.inputInt("Please enter the value for the element " + i + ":");
         }
         return array;
-   }
+    }
 
-   public void printArray(int [] array){
-        for(int item : array){
+    public void printArray(int[] array) {
+        for (int item : array) {
             System.out.print(item + " ");
         }
-   }
+    }
 }
